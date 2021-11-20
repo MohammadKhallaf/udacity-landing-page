345409865b6a9f1b04937d3a5b439b280cac1016
MohammedKhallaf <mohammed.khallaf.1997@gmail.com>
2021-11-21 00:39:59 +0200

    added the interactivity of sections' scroll

--- js/app.js
+++ js/app.js
@@ -1,79 +1,59 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
+"use strict";
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
- */
-
-/**
- * Define Global Variables
- *
- */
-
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
-// build the nav
-
-// Add class 'active' to section when near top of viewport
-
-// Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
-
-/**_____________________________my Code is here */
-// get section names
+/*___Define elements' variables___*/
 const pageSections = document.querySelectorAll("[data-nav]");
+const navBarList = document.getElementById("navbar__list");
 const sectionNames = [];
 const sectionIDs = [];
+
+/**
+ * ___Buid arrays of page section names and IDs___
+ * Section Name to be used in Nav bar Name
+ * Section ID to be used in linking to the section
+ */
+
 pageSections.forEach(function (section) {
   sectionNames.push(section.getAttribute("data-nav"));
   sectionIDs.push(section.id);
 });
 
-// get the nav bar list element
-const navBarList = document.getElementById("navbar__list");
+/**
+ * Build the Nav Bar from section IDs as anchors in list items
+ */
 
-for (i in sectionNames) {
-  console.log(sectionNames[i],sectionIDs[i]);
-  let R = document.createElement("li");
-  let L = document.createElement("a");
-  L.setAttribute("href", "#"+sectionIDs[i]);
-  L.classList.add("menu__link");
-  L.innerHTML = sectionNames[i];
-  
-  R.appendChild(L);
-  navBarList.appendChild(R);
+function createNavigationBar() {
+  for (let i in sectionNames) {
+    console.log(sectionNames[i], sectionIDs[i]);
+    let R = document.createElement("li");
+    let L = document.createElement("a");
+    L.setAttribute("href", "#" + sectionIDs[i]);
+    L.classList.add("menu__link");
+    L.innerHTML = sectionNames[i];
+
+    R.appendChild(L);
+    navBarList.appendChild(R);
+  }
 }
+
+/**
+ * Building the Nav bar on windows loading
+ */
+ window.onload = createNavigationBar();
+
+
+/**
+ * Adding the active class behaviour
+*/
+
+window.addEventListener("scroll", () => {
+  pageSections.forEach(function (section) {
+    let sectionTop = section.getBoundingClientRect().top;
+    let thirdOfWindow = Math.floor(window.innerHeight / 4);
+    if (sectionTop < thirdOfWindow && sectionTop >= -thirdOfWindow) {
+      console.log(section, sectionTop);
+      section.classList.add("activated-section");
+    } else {
+      section.classList.remove("activated-section");
+    }
+  });
+});
