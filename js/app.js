67ce289f4b97b7a0565226c822eeb7d02b6f2500
MohammedKhallaf <mohammed.khallaf.1997@gmail.com>
2021-11-21 02:17:02 +0200

    added two behaviours of hiding the scroll bar

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const pageSections = document.querySelectorAll("[data-nav]");
 const navBarList = document.getElementById("navbar__list");
 const sectionNames = [];
 const sectionIDs = [];
-
+let timeOut = 0;
 /**
  * ___Buid arrays of page section names and IDs___
  * Section Name to be used in Nav bar Name
@@ -35,30 +35,65 @@ function createNavigationBar() {
 }
 
 /**
- * Building the Nav bar on windows loading
- */
-window.onload = createNavigationBar();
-
-/**
  * Adding the active class behaviour
+ * and the active tab behaviour
  */
 
-window.addEventListener("scroll", () => {
+function setBehaviour() {
   pageSections.forEach(function (section) {
     let sectionTop = section.getBoundingClientRect().top;
-    let thirdOfWindow = Math.floor(window.innerHeight / 4);
+    let thirdOfWindow = Math.floor(window.innerHeight / 3);
+
     if (sectionTop < thirdOfWindow && sectionTop >= -thirdOfWindow) {
       section.classList.add("activated-section");
+      //tab-behaviour
       document.querySelector('a[href="#' + section.id + '"]').style.color =
         "orange";
       document.querySelector('a[href="#' + section.id + '"]').style.background =
         "#333";
     } else {
       section.classList.remove("activated-section");
+      //tab-behaviour
       document.querySelector('a[href="#' + section.id + '"]').style.color =
         "black";
       document.querySelector('a[href="#' + section.id + '"]').style.background =
         "#fff";
     }
   });
+}
+
+/**
+ * Hide the navigation bar after some time of no scrolling
+ */
+function hideOnTime() {
+  document.querySelector(".page__header").style.top = "0";
+  clearTimeout(timeOut);
+  timeOut = setTimeout(function () {
+    document.querySelector(".page__header").style.top = "-5rem";
+  }, 2000);
+}
+/**
+ * Hide the navigation bar for scrolling to up
+ */
+function hideOnScroll(afterScroll){
+  if (beforeScorll > afterScroll) {
+    document.querySelector(".page__header").style.top = "0";
+  } else {
+    document.querySelector(".page__header").style.top = "-5rem";
+  }
+  beforeScorll = afterScroll;
+}
+/**
+ * Building the Nav bar on windows loading
+ */
+window.onload = createNavigationBar();
+var beforeScorll = window.pageYOffset;
+
+window.addEventListener("scroll", () => {
+  setBehaviour();
+  hideOnTime();
+
+  let afterScroll = window.pageYOffset;
+  hideOnScroll(afterScroll)
+  
 });
